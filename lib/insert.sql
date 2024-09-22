cdb42472ba9d15d0251e2452f5e40cf3de3fbaf7
gideonkkk <gideon.kariuki@student.moringaschool.com>
2024-09-23 00:03:28 +0300

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 10, 'M', 'Brown', 'Friendly', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 7, 'M', 'Black', 'Aggressive', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 3, 'F', 'Brown', 'Calm', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 8, 'M', 'Brown', 'Brave', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 5, 'F', 'Blonde', 'Playful', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 12, 'M', 'Gray', 'Grumpy', false);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 6, 'F', 'White', 'Gentle', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 4, 'M', 'Black', 'Curious', true);
